eb3fa7cfbc41a8e612d4179150c9a2fa22565387
Amir Farmani <afarmani1a@gmail.com>
2020-08-09 15:28:56 -0400

    add BB support

--- python-client/history/retrieve.py
+++ python-client/history/retrieve.py
@@ -51,3 +51,28 @@ class RetrieveHistory:
         self.df['Close'] = self.df['Close'].astype(float)
         self.df['Volume'] = self.df['Volume'].astype(float)
         self.df['Number Of Trades'] = self.df['Number Of Trades'].astype(int)
+
+    def set_bb(self, window, deviation, datatype):
+        # dataType should be open high low close volume etc.
+        # window int, deviation int, datatype string
+        basis = self.set_ma_key(datatype, window)
+        # set MA
+        self.set_ma(window, datatype)
+        std = str(window) + 'STD' + str(datatype)
+        upper = str(window) + 'UpperBand' + str(datatype)
+        lower = str(window) + 'LowerBand' + str(datatype)
+
+        self.df[std] = self.df[datatype].rolling(window=window).std()
+        self.df[upper] = self.df[basis] + (self.df[std]*deviation)
+        self.df[lower] = self.df[basis] - (self.df[std]*deviation)
+
+        return self.df
+
+    def set_ma_key(self, datatype, window):
+        return str(window) + 'MA' + str(datatype)
+
+    def set_ma(self, window, datatype):
+        basis = self.set_ma_key(datatype, window)
+        self.df[basis] = self.df[datatype].rolling(window=window).mean()
+
+
--- python-client/services/socket.py
+++ python-client/services/socket.py
@@ -1,5 +1,6 @@
 from binance.websockets import BinanceSocketManager
 # twisted is the framework used to interact with a websocket
+from datetime import datetime
 
 from model.klineticker import KlineTicker
 from services.setup import ClientSetup
@@ -12,9 +13,9 @@ class BinanceSocket(ClientSetup):
         self.bsm = None
         self.conn_key = None
         self.new_data_available = True
-        # self.old_start = None
-        # self.curr_start = None
         self.ticker = None
+        self.condition = None
+        self.trans_range = None
 
     def connect(self):
         self.bsm = BinanceSocketManager(self.client)
@@ -30,18 +31,27 @@ class BinanceSocket(ClientSetup):
         self.ticker = KlineTicker(msg)
         print(self.ticker.__str__())
         self.new_data_available = True
-        # self.curr_start = self.ticker.get_start()
-        # self.update_new_data_available()
+        self.process_order()
 
-    # def update_new_data_available(self):
-    #     if self.old_start is None or self.curr_start > self.old_start:
-    #         self.new_data_available = True
-    #         self.old_start = self.curr_start
-    #     else:
-    #         self.new_data_available = False
+    def process_order(self):
+        close = float(self.ticker.close)
+        if self.condition == 'buy':
+            if close >= self.trans_range:
+                print(datetime.now(), 'BUY', self.condition, self.trans_range)
+        elif self.condition == 'sell':
+            if close <= self.trans_range:
+                print(datetime.now(), 'SELL', self.condition, self.trans_range)
+        else:
+            print(datetime.now(), 'NO ORDERS', self.condition, self.trans_range)
 
     def set_new_data_available(self, bol_value):
         self.new_data_available = bol_value
 
     def get_new_data_available(self):
         return self.new_data_available
+
+    def set_market_conditions(self, condition):
+        self.condition = condition
+
+    def set_trans_range(self, trans_range):
+        self.trans_range = trans_range
